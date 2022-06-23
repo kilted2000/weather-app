e78ead83d5613af2c66eb9e2dd08176d182c3eed
Richard Whittington <richardlwhittington@gmail.com>
2022-06-22 20:44:12 -0500

    finally can commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/*const API_KEY = "0ec221dcabaa5970b5589ff28f51b0a0";
+const API_URL =`http://api.openweathermap.org/data/2.5`;
+
+fetch('http://api.openweathermap.org/data/2.5')
+
+
+const API_KEY = "0ec221dcabaa5970b5589ff28f51b0a0";
+const API_URL = `http://api.openweathermap.org/data/2.5`;
+
+async function getForcast(city="memphis"){
+    const response = await fetch(`${API_URL}/weather?q=${city}&appid=${API_KEY}&units=imperial`);
+    const data = await response.json();
+    console.log(data);
+}
+getForcast();*/
